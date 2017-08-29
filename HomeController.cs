1c7460d85f99d153ce9e5dd762a4c229baf05f5e
Krystian412 <krystianv15@wp.pl>
2017-08-29 19:57:20 +0200

    Model
    Nie moge połączyć się z bazą , przeanalizować

---
+++ HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class HomeController : Controller
+    {  // dodanie metody listującej 
+        private NuntiusDBEntities m_oNuntiusDBEntities = null;
+
+        public HomeController()
+        {
+            m_oNuntiusDBEntities = new NuntiusDBEntities();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult List()
+        {
+            // nie wiem co dalej napisać , nie działa to 
+            var oPersons = from oPerson in m_oNuntiusDBEntities.Warnings
+                           select oPerson;
+            return View(oPersons.ToList());
+        }
+
+
+    }
+}
