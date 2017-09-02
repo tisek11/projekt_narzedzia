b80427315bdd4249ef415c7dbed2e03691a53ed6
Krystian412 <krystianv15@wp.pl>
2017-09-02 15:30:21 +0200

    Wygląd strony
    Podstawa strony jak ma wyglądac, dodanie klasy Kontakt oraz modyfikacja index

--- HomeController.cs
+++ HomeController.cs
@@ -1,40 +1,36 @@
-﻿using System;
+﻿using Strona_2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Vidly.Models;
 
-namespace Vidly.Controllers
+namespace Strona_2.Controllers
 {
     public class HomeController : Controller
-    {  // dodanie metody listującej 
-        private NuntiusDBEntities m_oNuntiusDBEntities = null;
-
-        public HomeController()
-        {
-            m_oNuntiusDBEntities = new NuntiusDBEntities();
-        }
-
+    {
         public ActionResult Index()
         {
+            var kontakty = new List<Kontakt>
+
+            {
+           new Kontakt{ Imie="Krystian " ,Nazwisko="Bąkowski " ,Miejscowosc="Bydgoszcz ",Ulica="Akademicka" },
+            
+        
+            };
+
+            ViewBag.Kontakty = kontakty;
+
+
             return View();
         }
 
-        public ActionResult About()
+
+        public ActionResult Contact()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
-        public ActionResult List()
-        {
-            // nie wiem co dalej napisać , nie działa to 
-            var oPersons = from oPerson in m_oNuntiusDBEntities.Warnings
-                           select oPerson;
-            return View(oPersons.ToList());
-        }
-
-
     }
 }
---
+++ Kontakt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Strona_2.Models
+{
+    public class Kontakt
+    {
+        public string Imie { get; set; }
+
+        public string Nazwisko { get; set; }
+
+        public string Miejscowosc { get; set; }
+
+        public string Ulica { get; set; }
+
+
+     }
+}
